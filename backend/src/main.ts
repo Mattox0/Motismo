305bd5d7ca462e2729053a1b91dd83d8184dc546
Mattox0 <matbegaaar@gmail.com>
2025-08-04 19:20:55 +0200

    feat(deploy): add pipelines to tests every branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,18 +14,6 @@ export async function bootstrap() {
     },
   });
 
-  const microserviceOptions: MicroserviceOptions = {
-    transport: Transport.REDIS,
-    options: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT ? +process.env.REDIS_PORT : 6379,
-      username: process.env.REDIS_USERNAME,
-      password: process.env.REDIS_PASSWORD,
-    },
-  };
-
-  app.connectMicroservice(microserviceOptions);
-
   // SWAGGER
   const config = new DocumentBuilder()
     .setTitle("Motismo")
